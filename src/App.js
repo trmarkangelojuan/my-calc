daeb3c56136a5a09fde30f112e5d33e446ed406c
mrkangelo <mrkngljn@gmail.com>
2023-01-30 14:15:44 +0800

    layout done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Button({val}){
+  return (
+    <div>
+      <button className='button'>{val}</button>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className='displaySection'></div>
+
+      <div className='buttonsSection'>
+        <Button val='/'/>
+        <Button val={9}/>
+        <Button val={8}/>
+        <Button val={7}/>
+        <Button val='*'/>
+        <Button val={6}/>
+        <Button val={5}/>
+        <Button val={4}/>
+        <Button val='-'/>
+        <Button val={3}/>
+        <Button val={2}/>
+        <Button val={1}/>
+        <Button val='+'/>
+        <Button val='='/>
+        <Button val={0}/>
+        <Button val='clr'/>
+      </div>
     </div>
   );
 }
