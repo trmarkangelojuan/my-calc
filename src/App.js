712f56018f254d7830230505412f3a2a52816163
mrkangelo <mrkngljn@gmail.com>
2023-01-31 14:09:15 +0800

    latest

--- src/App.js
+++ src/App.js
@@ -12,50 +12,72 @@ function Button({val, onclick}){
 function Display({disp}){
   return (
     <div className='displaySection'>
-        <input value={disp} type={'text'}/>
-      </div>
+      <input value={disp} type={'text'}/>
+    </div>
+  );
+}
+
+function DisplayOpe({disp2}){
+  return (
+    <div className='displaySection'>
+      <input value={disp2} type={'text'}/>
+    </div>
   );
 }
 
 
 export default function App() {
   const [disp, setDisp] = useState('0');
+  const [disp2, setDisp2] = useState('');
+  const [showOpe, setShowOpe] = useState(false);
+  const [showNum, setShowNum] = useState(true);
   const [num1, setNum1] = useState(0);
   const [operator, setOpe] = useState('');
   const [isClick, setClick] = useState(false);
   const [isAnswered, setAnswered] = useState(false);
+  const [isZero, setZero] = useState(true);
+  const [isRepeat, setRepeat] = useState(false);
 
   const showNumber = (e) =>{
     e.preventDefault();
-    let oldValue;
-    let current = disp + " ";
+    let oldValue, newValue;
 
-    if(parseInt(disp) > 0){
-      oldValue = current.replace(/[error ]/g, "");
-    } else {
-      if(isClick){
-        oldValue = current.replace(/[-error0+*/ ]/g, "");
-      } else{
-        oldValue = current.replace(/[0error ]/g, "");
-      }
-    }
-
+    if(isClick){
+      setShowNum(true);
+      setShowOpe(false);
+    } 
     
-    let newValue = e.target.innerHTML;
-    let finalDisp = oldValue + newValue; 
-    setDisp(finalDisp);
-    setAnswered(false);
+    if (isZero){
+      oldValue = parseInt(disp);
+      newValue = parseInt(e.target.innerHTML);
+      if(newValue == 0){
+        setZero(true);
+      } else {
+        setZero(false);
+      }
+    } else {
+      oldValue = disp;
+      newValue = e.target.innerHTML;
+    }
+    
+    setDisp(oldValue + newValue);
+    console.log(oldValue + newValue);
   }
 
   const showOperator = (e) =>{
     e.preventDefault();
+    let op = e.target.innerHTML;
+    
     if(!isClick){
-      let num = parseFloat(disp);
-      setNum1(num);
-      let op = e.target.innerHTML;
-      setDisp(op);
-      setOpe(op);
       setClick(true);
+      let num = disp;
+      setNum1(num);
+      setDisp2(op);
+      setOpe(op);
+      setDisp('0');
+      setZero(true);
+      setShowNum(false);
+      setShowOpe(true);
       setAnswered(false);
     }
   }
@@ -74,14 +96,10 @@ export default function App() {
       } else if(operator === '-'){
         ans = num1 - num2;
       }
-
-      if(!isFinite(ans) || isNaN(ans)){
-        setDisp('error');
-        setAnswered(true);
-      } else {
-        setDisp(ans);
-        setAnswered(true);
-      }
+      
+      setDisp(parseFloat(ans));
+      setAnswered(true);
+      setRepeat(true);
     }
   }
 
@@ -89,13 +107,24 @@ export default function App() {
     setDisp('0');
     setClick(false);
     setNum1(0);
+    setZero(true);
+    setShowNum(true);
+    setShowOpe(false);
+    setOpe('');
+    setAnswered(false);
+    setDisp2('');
   }
 
+
+  //var arr = ["/", "9", "8", "7", "*" ,"6", "5", "4", "-","3","2","1", "+", "=", "0", "clr"];    
+    
   return (
     <div className="body">
 
-      <Display disp={disp}/>
+      {showNum && <Display disp={disp}/>}
       
+      {showOpe && <DisplayOpe disp2={disp2}/>}
+
       <div className='buttonsSection'>
         <Button val='/' onclick={showOperator}/>
         <Button val={'9'} onclick={showNumber}/>
