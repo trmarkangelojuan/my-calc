038e6e566f7d43a49cc7971bba97a8be01049288
mrkangelo <mrkngljn@gmail.com>
2023-01-31 15:40:41 +0800

    latest

--- src/App.js
+++ src/App.js
@@ -17,114 +17,75 @@ function Display({disp}){
   );
 }
 
-function DisplayOpe({disp2}){
-  return (
-    <div className='displaySection'>
-      <input value={disp2} type={'text'}/>
-    </div>
-  );
-}
-
 
 export default function App() {
-  const [disp, setDisp] = useState('0');
-  const [disp2, setDisp2] = useState('');
-  const [showOpe, setShowOpe] = useState(false);
-  const [showNum, setShowNum] = useState(true);
+
+  const [disp, setDisp] = useState('');
   const [num1, setNum1] = useState(0);
-  const [operator, setOpe] = useState('');
-  const [isClick, setClick] = useState(false);
-  const [isAnswered, setAnswered] = useState(false);
-  const [isZero, setZero] = useState(true);
-  const [isRepeat, setRepeat] = useState(false);
+  const [operation, setOper] = useState('');
+  const [isClicked, setClick] = useState(false);
 
   const showNumber = (e) =>{
     e.preventDefault();
     let oldValue, newValue;
 
-    if(isClick){
-      setShowNum(true);
-      setShowOpe(false);
-    } 
-    
-    if (isZero){
-      oldValue = parseInt(disp);
-      newValue = parseInt(e.target.innerHTML);
-      if(newValue == 0){
-        setZero(true);
-      } else {
-        setZero(false);
-      }
+    if(isClicked){
+      oldValue = '';
+      setClick(false);
     } else {
       oldValue = disp;
-      newValue = e.target.innerHTML;
     }
-    
-    setDisp(oldValue + newValue);
-    console.log(oldValue + newValue);
+    newValue = e.target.innerHTML;
+    setDisp(parseInt(oldValue + newValue));
   }
 
   const showOperator = (e) =>{
     e.preventDefault();
-    let op = e.target.innerHTML;
-    
-    if(!isClick){
+
+    if (disp === ''){
+      setDisp('');
+    } else {
+      let op = e.target.innerHTML;
+      setNum1(disp);
+      setOper(op);
+      setDisp(op);
       setClick(true);
-      let num = disp;
-      setNum1(num);
-      setDisp2(op);
-      setOpe(op);
-      setDisp('0');
-      setZero(true);
-      setShowNum(false);
-      setShowOpe(true);
-      setAnswered(false);
     }
   }
 
   const compute = (e) => {
-    if (!isAnswered){
-      setClick(false);
-      let num2 = parseFloat(disp);
-      let ans;
-      if(operator === '*'){
-        ans = num1 * num2;
-      } else if(operator === '/'){
-        ans = num1 / num2;
-      } else if(operator === '+'){
-        ans = num1 + num2;
-      } else if(operator === '-'){
-        ans = num1 - num2;
-      }
+    
+    if (!isClicked){
+      let num, num2, ans;
+      num2 = parseFloat(disp);
+      num = parseFloat(num1);
       
-      setDisp(parseFloat(ans));
-      setAnswered(true);
-      setRepeat(true);
+      if(operation === '*'){
+        ans =  num * num2;
+      } else if(operation === '/'){
+        ans = num / num2;
+      } else if(operation === '+'){
+        ans = num + num2;
+      } else if(operation === '-'){
+        ans = num - num2;
+      }
+
+      setDisp(ans);
     }
+    
   }
 
   const clearDisplay = () => {
-    setDisp('0');
-    setClick(false);
-    setNum1(0);
-    setZero(true);
-    setShowNum(true);
-    setShowOpe(false);
-    setOpe('');
-    setAnswered(false);
-    setDisp2('');
+    setDisp('');
   }
-
 
   //var arr = ["/", "9", "8", "7", "*" ,"6", "5", "4", "-","3","2","1", "+", "=", "0", "clr"];    
     
   return (
     <div className="body">
 
-      {showNum && <Display disp={disp}/>}
+      <Display disp={disp}/>
       
-      {showOpe && <DisplayOpe disp2={disp2}/>}
-
       <div className='buttonsSection'>
         <Button val='/' onclick={showOperator}/>
         <Button val={'9'} onclick={showNumber}/>
